a31e8b334081bb56e236ec5ffde540941dbc99f8
Altug Heper <altugheper@gmail.com>
2023-01-27 22:38:15 +0100

    27/01/23

---
+++ src/main/java/com/tpe/SpbootlibraryprojectApplication.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbootlibraryprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbootlibraryprojectApplication.class, args);
+	}
+
+}
